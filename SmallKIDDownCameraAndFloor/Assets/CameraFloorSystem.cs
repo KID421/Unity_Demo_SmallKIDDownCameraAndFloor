e9d984a5c6e6eafe75a84bb52bc15f41b574a3ef
KID <50666207+KID421@users.noreply.github.com>
2020-07-31 13:01:05 +0800

    網頁

---
+++ SmallKIDDownCameraAndFloor/Assets/CameraFloorSystem.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraFloorSystem : MonoBehaviour
+{
+    public int countFloor;
+    public Text textCount;
+
+    private void Update()
+    {
+        Move();
+        Count();
+    }
+
+    private void Move()
+    {
+        transform.Translate(0, Time.deltaTime * -2, 0);
+    }
+
+    /// <summary>
+    /// 判斷層數
+    /// </summary>
+    private void Count()
+    {
+        // 層數 = 絕對值((轉整數)攝影機.Y 軸 / 2)
+        // - 攝影機 Y 軸取絕對值當作層數
+        // - / 2 讓層數變慢，可自行調整
+        countFloor = Mathf.Abs((int)transform.position.y / 2);
+        textCount.text = "層數：" + countFloor;
+    }
+}
---
+++ SmallKIDDownCameraAndFloor/Assets/FloorSystem.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class FloorSystem : MonoBehaviour
+{
+    public Transform floor;
+
+    private int y = -5;
+
+    private void Awake()
+    {
+        Screen.SetResolution(450, 800, false);
+    }
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnFloor", 0, 0.5f);
+    }
+
+    private void SpawnFloor()
+    {
+        int x = Random.Range(-3, 4);
+        Vector3 pos = new Vector3(x, y, 0);
+        y-=2;
+        Instantiate(floor, pos, Quaternion.identity);
+    }
+}
